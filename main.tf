437d86f960e15ad3c65284559e3367f2c3acab82
sakshi <patilsakshi7158@gmail.com>
2025-05-13 15:58:47 +0530

    change values11110

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ data "aws_security_group" "launch-wizard-11" {
 }
 
 resource "aws_instance" "terraform" {
-  ami           = "ami-0c1ac8a41498c1a9c"
+  ami = "ami-0c1ac8a41498c1a9c"
   instance_type = var.instance_type
   key_name = "newins"
   vpc_security_group_ids = ["data.aws_security_group.launch-wizard-11"]
