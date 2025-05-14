c3de37fd45de5326dfe3a64900fc00610ec4a377
sakshi <patilsakshi7158@gmail.com>
2025-05-14 07:35:55 +0530

    this is terraforma file11

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "aws_security_group" "my_sec_group" {
     description = "TLS from VPC"
     from_port   = 22
     to_port     = 22
-    protocol    = "ssh"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
